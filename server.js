5301d6f5640a3d3dc845afb15049c38bda27eea6
Zeel Patel <zeelpatel265512@gmail.com>
2022-03-23 12:25:17 +0530

    Added API & Model

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
-                                                         
+//Connect DB
+connectDB();                                                         
 app.get('/', (req, res) => res.send ('API Running'));
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
 
 const PORT = process.env. PORT || 5000;
 
