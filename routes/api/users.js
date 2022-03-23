b79756f781b277e3ce2196cab7b267c82b034fcb
Zeel Patel <zeelpatel265512@gmail.com>
2022-03-23 15:25:38 +0530

    User Auth & Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar') ;
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check,validationResult}=require('express-validator');
 const User=require('../../models/User');
 
@@ -35,12 +37,23 @@ router.post('/',[
         const salt=await bcrypt.genSalt(10);
         user.password=await bcrypt.hash(password,salt);
         await user.save();
-        res.send('User registered');
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtToken'),
+            { expiresIn:360000},
+            (err,token)=>{
+                if (err) throw err;
+                res.json({token});
+            });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
     }
-    res.send('User route');
 });
 
 module.exports=router;
